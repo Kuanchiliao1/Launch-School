fe3f064500ac319317e8fd2bd60c4daa448a45d3
kuanchiliao1 <kuanchiliao@DESKTOP-M1Q2026.localdomain>
2022-06-15 18:35:04 -0700

    Finish ex2

---
+++ age.rb
@@ -0,0 +1,12 @@
+
+puts "How old are you?"
+
+name = gets.chomp
+
+name1 = name + 1 
+name2 = name + 2
+name3 = name + 3
+
+"In 10 years you'll be" + name3.to_s   
+
+"In 10 years you'll be" + name2.to_s
